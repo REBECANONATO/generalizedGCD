59ef3e6d43dde1c9db54ed29a1b1e3b59aa0314a
Rebeca <rebeca.nonato.lemaf@gmail.com>
2020-08-19 14:12:54 -0300

    initial

---
+++ generalizedGCD/GCD.cs
@@ -0,0 +1,47 @@
+﻿public class GCD
+{
+    public static int generalizedGCD(int num, int[] arr)
+    {
+        bool contrl = false;
+        int[] newArr = null;
+
+        newArr = selectionSort(arr);
+
+        int numMax = newArr[newArr.Length - 1];
+
+        while (!contrl)
+        {
+            contrl = true;
+            for (int i = 0; i < num; i++)
+            {
+                if (newArr[i] % numMax != 0)
+                {
+                    contrl = false;
+                    break;
+                }
+            }
+            if (!contrl)
+                numMax -= 1;
+        };
+        return numMax;
+    }
+
+    public static int[] selectionSort(int[] vetor)
+    {
+        int min, aux;
+        for (int i = 0; i < vetor.Length - 1; i++)
+        {
+            min = i;
+            for (int j = i + 1; j < vetor.Length; j++)
+                if (vetor[j] < vetor[min])
+                    min = j;
+            if (min != i)
+            {
+                aux = vetor[min];
+                vetor[min] = vetor[i];
+                vetor[i] = aux;
+            }
+        }
+        return vetor;
+    }
+}
---
+++ generalizedGCD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace generalizedGCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 20, 5, 25, 80, 90 };
+
+            Console.WriteLine(GCD.generalizedGCD(5, arr));
+
+        }
+        
+    }
+}
